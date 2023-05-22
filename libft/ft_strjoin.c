2ec6037ad62a4261f70104310613c3fec799f41d
lubuper <lubu.p@hotmail.com>
2023-05-22 01:20:49 +0100

    norm checks

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,38 +6,36 @@
 /*   By: lde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:53:09 by lde-sous          #+#    #+#             */
-/*   Updated: 2022/11/10 16:06:08 by lde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/22 01:20:06 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(const char *s1, const char *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
-    int i = 0;
-    int j = 0;
-    char *allocated;
+	int		i;
+	int		j;
+	char	*allocated;
 
-    if (!s1 || !s2)
-        return NULL;
-
-    allocated = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!allocated)
-        return NULL;
-
-    while (s1[i])
-    {
-        allocated[i] = s1[i];
-        i++;
-    }
-
-    while (s2[j])
-    {
-        allocated[i] = s2[j];
-        i++;
-        j++;
-    }
-
-    allocated[i] = '\0';
-    return allocated;
+	if (!s1 || !s2)
+		return (NULL);
+	allocated = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!allocated)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		allocated[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		allocated[i] = s2[j];
+		i++;
+		j++;
+	}
+	allocated[i] = '\0';
+	return (allocated);
 }
--- source/lexer.c
+++ source/lexer.c
@@ -6,7 +6,7 @@
 /*   By: alexfern <alexfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:15:45 by lde-sous          #+#    #+#             */
-/*   Updated: 2023/05/18 18:30:333 by alexfern         ###   ########.fr       */
+/*   Updated: 2023/05/22 01:13:24 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,28 @@
     feedback or return values accordingly.
 */
 
-void command(const char *input, t_attr *att, char **envp)
+void	command(const char *input, t_attr *att, char **envp)
 {
-    char **tok;
-    (void) envp;
-    
-    if (!att->tok_arr[0])
-        return;
-    tok = att->tok_arr;
-    if (!ft_strcmp(att->tok_arr[0], "echo"))
-        echo(*att);
-    else if (ft_strcmp(tok[0], "pwd") == 0)
-        pwd();
-    else if (ft_strcmp(tok[0], "cd") == 0)
-        cd(att);
-    else if (ft_strcmp(tok[0], "export") == 0)
-        export(att);
-    else if (ft_strcmp(tok[0], "unset") == 0)
-        unset(att);
-    else if (ft_strcmp(tok[0], "env") == 0)
-        env(att);
-    else if (ft_strcmp(tok[0], "exit") == 0)
-        return ft_exit(input, *att);
-    else if (!input[0])
-        readline("\033[0;32mminishell$\033[0m ");
+	char	**tok;
+
+	(void)envp;
+	if (!att->tok_arr[0])
+		return ;
+	tok = att->tok_arr;
+	if (!ft_strcmp(att->tok_arr[0], "echo"))
+		echo(*att);
+	else if (ft_strcmp(tok[0], "pwd") == 0)
+		pwd();
+	else if (ft_strcmp(tok[0], "cd") == 0)
+		cd(att);
+	else if (ft_strcmp(tok[0], "export") == 0)
+		export(att);
+	else if (ft_strcmp(tok[0], "unset") == 0)
+		unset(att);
+	else if (ft_strcmp(tok[0], "env") == 0)
+		env(att);
+	else if (ft_strcmp(tok[0], "exit") == 0)
+		return (ft_exit(input, *att));
+	else if (!input[0])
+		readline("\033[0;32mminishell$\033[0m ");
 }
