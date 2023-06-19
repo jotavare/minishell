0607cd956592c25faece70431207a429c4ebe783
jpalmeidaalves <jpalmeidaalves@hotmail.com>
2023-06-19 13:51:01 +0100

    undo changes in check_next_step

--- minishell/sources/minishell.c
+++ minishell/sources/minishell.c
@@ -40,32 +40,27 @@ int check_next_step(t_attr *att, int *i)
 	att->redir = 0;
 	att->read_from_pipe = 0;
 	att->read_from_file = 0;
-
-	/* if (is_symbol(att->commands_arr[*i]))
-		*i = *i + 1;
-	if (*i > 0 && !ft_strcmp(att->commands_arr[*i - 1], ">") && !ft_strcmp(att->commands_arr[*i - 1], ">")  )
-		*i = *i + 1; */
 	
-	if (*i > 1 && is_symbol(att->commands_arr[*i - 1]))
-	{
-		if (!ft_strcmp(att->commands_arr[*i - 1], "|"))
-			att->read_from_pipe = 1;
-		if (!ft_strcmp(att->commands_arr[*i - 1], ">") || !ft_strcmp(att->commands_arr[*i - 1], ">>"))
-			*i = *i + 1;
-		else if (!ft_strcmp(att->commands_arr[*i - 1], "<") || !ft_strcmp(att->commands_arr[*i - 1], "<<"))
-			*i = *i + 2;
-	}
-
 	if (att->commands_arr[*i + 1] && is_symbol(att->commands_arr[*i + 1]))
 	{
 		if (!ft_strcmp(att->commands_arr[*i + 1], "|"))
 			att->write_to_pipe = 1;
 		else if (!ft_strcmp(att->commands_arr[*i + 1], ">") || !ft_strcmp(att->commands_arr[*i + 1], ">>"))
-			att->redir = 1;
+		 	att->redir = 1;
 		else if (!ft_strcmp(att->commands_arr[*i + 1], "<"))
-			att->read_from_file = 1;
+		 	att->read_from_file= 1;
 	}
-
+	if (*i > 1 && is_symbol(att->commands_arr[*i - 1]))
+	{
+		if (!ft_strcmp(att->commands_arr[*i - 1], "|"))
+		{
+			att->read_from_pipe = 1;
+		}
+		if (!ft_strcmp(att->commands_arr[*i - 1], ">") || !ft_strcmp(att->commands_arr[*i - 1], ">>"))
+		 	*i = *i + 1;
+		else if (!ft_strcmp(att->commands_arr[*i - 1], "<") || !ft_strcmp(att->commands_arr[*i - 1], "<<"))
+		 	*i = *i + 1;
+	}
 	return 0;
 }
 
